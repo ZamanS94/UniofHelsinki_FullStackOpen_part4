d2d4c6aaf24b454535e42a4a3459b339681a7012
Sabina Zaman <sabina.zaman94@gmail.com>
2024-04-12 14:33:51 +0200

    4.4: Helper Functions and Unit Tests, step 2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,4 +1,9 @@
 export const dummy = (blogs) => {
     return 1
 }
- 
+
+export const totalLikes = (blogs) => {
+    return blogs.reduce((total, blog) => {
+        return total + blog.likes
+      }, 0)
+  }
