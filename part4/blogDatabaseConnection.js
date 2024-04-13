a0f9cf4f2bb1c26d166a8e2a1204e818f719fb27
Sabina Zaman <sabina.zaman94@gmail.com>
2024-04-13 15:31:52 +0200

    4.5*: Helper Functions and Unit Tests, step 3

--- part4/blogDatabaseConnection.js
+++ part4/blogDatabaseConnection.js
@@ -2,6 +2,6 @@ import mongoose from 'mongoose'
 
 export const connectToDatabase = () => {
   const password = process.argv[2]
-  const mongoUrl = `mongodb+srv://sabina37:${password}@cluster0.enthgpn.mongodb.net/`;
+  const mongoUrl = `mongodb+srv://sabina37:${password}@cluster0.enthgpn.mongodb.net/`
   mongoose.connect(mongoUrl)
 }
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -7,3 +7,24 @@ export const totalLikes = (blogs) => {
         return total + blog.likes
       }, 0)
   }
+
+export const favoriteBlog = (blogs) => {
+
+    let maxLikes = blogs[0].likes
+    let favoriteBlog = blogs[0]
+
+    for (let i = 1; i < blogs.length; i++) {
+      const currentLikes = blogs[i].likes
+      if (currentLikes > maxLikes) {
+        maxLikes = currentLikes
+        favoriteBlog = blogs[i]
+      }
+    }
+  
+    return {
+    title: favoriteBlog.title,
+    author: favoriteBlog.author,
+    likes: favoriteBlog.likes
+  }
+  }
+
